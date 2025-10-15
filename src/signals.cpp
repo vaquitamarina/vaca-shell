e51bd305a6d8427edb20757917cb376e6e27a12c
wiLLotheRock <wrcopajah@unjbg.edu.pe>
2025-10-15 02:05:40 -0500

    error corregido

--- src/signals.cpp
+++ src/signals.cpp
@@ -18,16 +18,15 @@ void SignalHandler::sigint_handler(int signo) {
     }
 }
 
-
-void SignalHandler::sigint_handler(int signo) {
+void SignalHandler::sigchld_handler(int signo) {
     (void)signo;
     
-    if (foreground_pid > 0) {
-        kill(foreground_pid, SIGINT);
+    int saved_errno = errno;
+    
+    while (waitpid(-1, nullptr, WNOHANG) > 0) {
     }
-    else {
-        write(STDOUT_FILENO, "\n", 1);
-    }
+    
+    errno = saved_errno;
 }
 
 void SignalHandler::setup_signals() {
@@ -56,15 +55,13 @@ void SignalHandler::restore_default_handlers() {
     sigaction(SIGTSTP, &sa_default, nullptr);
 }
 
-void SignalHandler::restore_default_handlers() {
-    struct sigaction sa_default;
-    memset(&sa_default, 0, sizeof(sa_default));
-    sa_default.sa_handler = SIG_DFL;
-    sigemptyset(&sa_default.sa_mask);
+void SignalHandler::ignore_sigint() {
+    struct sigaction sa_ignore;
+    memset(&sa_ignore, 0, sizeof(sa_ignore));
+    sa_ignore.sa_handler = SIG_IGN;
+    sigemptyset(&sa_ignore.sa_mask);
     
-    sigaction(SIGINT, &sa_default, nullptr);
-    sigaction(SIGCHLD, &sa_default, nullptr);
-    sigaction(SIGTSTP, &sa_default, nullptr);
+    sigaction(SIGINT, &sa_ignore, nullptr);
 }
 
 void SignalHandler::restore_sigint() {
