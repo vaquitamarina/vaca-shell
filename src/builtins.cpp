0c87a91eb75098c6665e2eadeb692483a54dcde9
wiLLotheRock <wrcopajah@unjbg.edu.pe>
2025-10-14 21:57:22 -0500

    agregando funcion is_builtin que reconoce si es un comando interno

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -5,3 +5,9 @@
 #include <limits.h>
 #include <errno.h>
 #include <cstring>
+
+bool Builtins::is_builtin(const std::string& cmd) const {
+    return cmd == "cd" || cmd == "pwd" || cmd == "exit" || cmd == "salir" ||
+        cmd == "help" || cmd == "ayuda" || cmd == "history" || cmd == "historial" ||
+        cmd == "alias" || cmd == "jobs" || cmd == "trabajos";
+}
